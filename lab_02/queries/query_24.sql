d0193742db4e91dd8aac2a24ef3ff9e57f84db04
kam16u810 <bokseng12@gmail.com>
2018-10-15 20:45:20 +0300

    change query 24

--- lab_02/queries/query_24.sql
+++ lab_02/queries/query_24.sql
@@ -1,3 +1,5 @@
 SELECT BuildId, CameraBodyId, FilterId, LensId, Price,
-SUM(Price) OVER(PARTITION BY CameraBodyId) AS Total
+SUM(Price) OVER(PARTITION BY CameraBodyId) AS Total,
+COUNT(Price) OVER(PARTITION BY CameraBodyId) AS Amount
 FROM CameraBuild
+ORDER BY CameraBodyId
