4a1d96d3c0b4a09c24fc239d68a72ad7be0833ac
kam16u810 <bokseng12@gmail.com>
2018-12-14 13:42:51 +0300

    rename old folder

--- lab_05/queries/query_15.sql
+++
@@ -1,5 +0,0 @@
--- print list of kits which have sum less than 500 dollars
-SELECT c1.BuildId, c2.Brand, c2.Model, c2.Color, SUM(c1.Price) AS 'Price'
-FROM CameraBuild c1 JOIN CameraBody c2 ON c1.BuildId = c2.CameraId
-GROUP BY BuildId, Brand, Model, Color
-HAVING SUM(Price) < 500
